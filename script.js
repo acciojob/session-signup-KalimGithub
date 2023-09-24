90edde4595c9a5134f4f7009d2379efce7d10149
kalim shaikh <117559118+KalimGithub@users.noreply.github.com>
2023-09-24 16:41:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 //your JS code here. If required.
+const name = document.getElementById("name");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const confirmPassword = document.getElementBydId("confirm-password");
