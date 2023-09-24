cdaeb37e9ca4304cb411fbca8b4e91db8e1802c6
kalim shaikh <117559118+KalimGithub@users.noreply.github.com>
2023-09-24 16:45:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,3 +3,9 @@ const name = document.getElementById("name");
 const email = document.getElementById("email");
 const password = document.getElementById("password");
 const confirmPassword = document.getElementBydId("confirm-password");
+const submit = document.getElementById("submit");
+
+
+submit.addEventListener("click", (event)=>{
+	
+})
